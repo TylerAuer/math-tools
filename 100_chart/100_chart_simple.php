49fcdf59f65488390f0cd32f27f22b0731b7fd7c
Tyler Auer <Tyler.Auer@gmail.com>
2019-12-22 11:40:03 -0800

    first commit

---
+++ 100_chart/100_chart_simple.php
@@ -0,0 +1,56 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!--Loads universal header-->
+  <?php include '../header.php';?>
+
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>100s Chart (Simple)</title>
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+  <!--  Local CSS  -->
+  <link rel="stylesheet" href="../css/styles.css">
+</head>
+
+<body>
+  <div class="container">
+    <!--  Title  -->
+    <div class="col-xs-12">
+      <p class="title-text text-center">100s Grid</p>
+      <p class="text-center subtitle-text">Beta Version by <a href='https://twitter.com/MathFireworks'>@MathFireworks</a></p>
+    </div>
+
+    <!--  Buttons  -->
+    <div id="btn-area" class="btn-bank" class="col-xs-12 text-center">
+      <div id="skip-count-btn-bank" class="text-center">
+        <button id="reset-btn" type="button" class="btn btn-primary">Reset</button>
+
+      </div>
+      <div id="function-btn-bank" class="text-center">
+      </div>
+    </div>
+
+    <!-- Table  -->
+    <div class="col-xs-12">
+      <div class='center-block'>
+        <table id='grid' class='table'>
+        </table>
+      </div>
+    </div>
+
+  </div>
+
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  <script src="../scripts/grid.js"></script>
+</body>
+
+</html>
