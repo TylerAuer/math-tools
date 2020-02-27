77b5fb742ffab86d207a8e8c0e27ba8e3521066f
Tyler Auer <Tyler.Auer@gmail.com>
2020-02-26 21:57:48 -0800

    working prototype without user customizability

--- assets/js/half-life.js
+++ assets/js/half-life.js
@@ -1,63 +1,106 @@
-// TODO: Add counter for generations
-// TODO: Stop process once all atoms are eliminated
+// TODO: Add fields for customizing the half-life
+// TODO: Add a geiger counter sound
+const sim = {
+  config: {
+    // these do NOT change when the simulation runs
+    startingAtomCount: 10000,
+    halfLifeInMs: 5000,
+    msBetweenDecayEvents: 100,
+    // these update as the simulation runs
+    decayEventCount: 0,
+    halfLifeCount: 0,
+    timeElapsed: 0
+  },
 
-const config = {
-  startingAtomCount: 10000,
-  //TODO: define getter setter for starting atom count to update description paragraph
-  halfLifeInMs: 5000,
-  msBetweenDecayEvents: 250
-};
-config.decayEventProbability = Math.pow(
-  0.5,
-  1 / (config.halfLifeInMs / config.msBetweenDecayEvents) // nth root where n the number of decay events / half life
-);
+  simHandle: 0, // ID for the setTimeout call
+  timerHandle: 0, // ID for the setInterval call
+  start: function() {
+    this.stop();
+    this.simHandle = setInterval(
+      () => decayEvent(),
+      this.config.msBetweenDecayEvents
+    );
+    this.timerHandle = setInterval(() => {
+      this.config.timeElapsed = (this.config.timeElapsed * 10 + 1) / 10;
+      const timer = document.getElementById("secElapsed");
+      timer.innerText = this.config.timeElapsed.toFixed(1);
+    }, 100);
+  },
 
-const simController = {
-  decayEventCount: 0,
-  halfLifeCount: 0,
-  timeElapsed: 0,
-  atomsLeft: config.startingAtomCount,
+  stop: function() {
+    if (this.simHandle) {
+      clearInterval(this.simHandle);
+      this.simHandle = 0;
+    }
+    if (this.timerHandle) {
+      clearInterval(this.timerHandle);
+      this.timerHandle = 0;
+    }
+  },
+
+  reset: function() {
+    this.stop();
+    const atomDiv = document.getElementById("atom-holder");
+    atomDiv.innerHTML = "";
+    for (let i = 0; i < sim.config.startingAtomCount; i++) {
+      const atomElem = document.createElement("div");
+      atomElem.classList = "atom atom-alive";
+      atomDiv.appendChild(atomElem);
+    }
+    this.config.halfLifeCount = 0;
+    this.config.timeElapsed = 0;
+    this.config.atomsLeft = this.config.startingAtomCount;
+    this.updateHeadings();
+    const timer = document.getElementById("secElapsed");
+    timer.innerText = this.config.timeElapsed.toFixed(1);
+  },
+
   updateHeadings: function() {
     const hlCounter = document.getElementById("hlCounter");
-    const secElapsed = document.getElementById("secElapsed");
     const atomsLeft = document.getElementById("atomsCounter");
-    hlCounter.innerHTML = this.halfLifeCount;
-    secElapsed.innerHTML = this.timeElapsed;
-    atomsLeft.innerHTML = this.atomsLeft;
+    hlCounter.innerHTML = this.config.halfLifeCount;
+    atomsLeft.innerHTML = this.config.atomsLeft;
   }
 };
 
+sim.config.decayEventProbability = Math.pow(
+  0.5,
+  1 / (sim.config.halfLifeInMs / sim.config.msBetweenDecayEvents) // nth root where n the number of decay events / half life
+);
+sim.config.atomsLeft = sim.config.startingAtomCount;
+sim.reset();
+
 const dotsOnPageText = document.getElementById("dots-on-page");
-dotsOnPageText.innerHTML = config.startingAtomCount;
+dotsOnPageText.innerHTML = sim.config.startingAtomCount;
 
-const atomDiv = document.getElementById("atom-holder");
-// TODO: Need to set an upper bound for halfLifeMs
-let atomsLeft = config.startingAtomCount;
-
-for (let i = 0; i < config.startingAtomCount; i++) {
-  const atomElem = document.createElement("div");
-  atomElem.classList = "atom atom-alive";
-  atomDiv.appendChild(atomElem);
-}
-
-setInterval(decayEvent, config.msBetweenDecayEvents);
+const startBtn = document.getElementById("start-btn");
+const stopBtn = document.getElementById("stop-btn");
+const resetBtn = document.getElementById("reset-btn");
+startBtn.addEventListener("click", () => sim.start());
+stopBtn.addEventListener("click", () => sim.stop());
+resetBtn.addEventListener("click", () => sim.reset());
 
 function decayEvent() {
-  simController.decayEventCount += 1;
+  // BUG: Only works when half-life is a multiple msBetweenDecayEvent
+  sim.config.decayEventCount += 1;
   if (
-    simController.decayEventCount %
-      (config.halfLifeInMs / config.msBetweenDecayEvents) ===
+    sim.config.decayEventCount %
+      (sim.config.halfLifeInMs / sim.config.msBetweenDecayEvents) ===
     0
   ) {
-    simController.halfLifeCount += 1;
+    sim.config.halfLifeCount += 1;
   }
 
-  document.querySelectorAll(".atom-alive").forEach(elem => {
-    if (Math.random() > config.decayEventProbability) {
+  aliveAtoms = document.querySelectorAll(".atom-alive");
+  if (aliveAtoms.length === 0) {
+    sim.stop();
+  }
+  aliveAtoms.forEach(elem => {
+    if (Math.random() > sim.config.decayEventProbability) {
       elem.classList = "atom atom-dead";
       elem.style = "background-color: white;";
-      simController.atomsLeft -= 1;
+      sim.config.atomsLeft -= 1;
     }
   });
-  simController.updateHeadings();
+  sim.updateHeadings();
 }
