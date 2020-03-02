87f747b22e0b204eda140176f3dba17c894eb772
Tyler Auer <Tyler.Auer@gmail.com>
2020-03-01 20:54:09 -0800

    add colors for winning squares to tic-tac-toe

---
+++ apps/tictactoe/precache-manifest.8dc11459423d74c64b67b2755503cd5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47facd60da0a568d262f942ad931db65",
+    "url": "/apps/tictactoe/index.html"
+  },
+  {
+    "revision": "cdaa2bc0571092dd5af3",
+    "url": "/apps/tictactoe/static/css/main.96562d7e.chunk.css"
+  },
+  {
+    "revision": "44cac84b209f70257e97",
+    "url": "/apps/tictactoe/static/js/2.bb21a5fe.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/apps/tictactoe/static/js/2.bb21a5fe.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cdaa2bc0571092dd5af3",
+    "url": "/apps/tictactoe/static/js/main.e15f26f5.chunk.js"
+  },
+  {
+    "revision": "98b5ec5460b2f8db37f3",
+    "url": "/apps/tictactoe/static/js/runtime-main.9f3f343e.js"
+  }
+]);
